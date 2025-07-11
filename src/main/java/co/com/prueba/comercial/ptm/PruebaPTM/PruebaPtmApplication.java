be47099e9556f244a0db0ad21a48a90763f9400e
edwinsillog <edwingordillo802@gmail.com>
2025-07-11 11:20:06 -0500

    Se sube proyecto creado por spring initializr

---
+++ src/main/java/co/com/prueba/comercial/ptm/PruebaPTM/PruebaPtmApplication.java
@@ -0,0 +1,13 @@
+package co.com.prueba.comercial.ptm.PruebaPTM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaPtmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaPtmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/prueba/comercial/ptm/PruebaPTM/PruebaPtmApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.prueba.comercial.ptm.PruebaPTM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaPtmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
